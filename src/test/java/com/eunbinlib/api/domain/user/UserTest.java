e6f59b417558fda895bde1aa5fcc93b0572520c4
JinWoo Park <jinwuui@gmail.com>
2022-11-29 10:05:23 +0900

    test: add test codes of Domain area (User, Member, Guest)

--- src/test/java/com/eunbinlib/api/domain/user/UserTest.java
+++ src/test/java/com/eunbinlib/api/domain/user/UserTest.java
@@ -1,4 +1,33 @@
 package com.eunbinlib.api.domain.user;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class UserTest {
+
+    @Test
+    @DisplayName("회원의 사용자 타입을 구하는 경우")
+    void getUserTypeOfMember() {
+        // given
+        Member member = Member.builder()
+                .nickname("닉네임")
+                .build();
+
+        // then
+        Assertions.assertThat(member.getUserType())
+                .isEqualTo("member");
+    }
+
+
+    @Test
+    @DisplayName("게스트의 사용자 타입을 구하는 경우")
+    void getUserTypeOfGuest() {
+        // given
+        Guest guest = Guest.builder().build();
+
+        // then
+        Assertions.assertThat(guest.getUserType())
+                .isEqualTo("guest");
+    }
 }
