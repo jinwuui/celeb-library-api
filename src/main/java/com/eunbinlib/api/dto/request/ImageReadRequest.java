207467a5d164a77913ca3fc09d216c2742eca378
JinWoo Park <jinwuui@gmail.com>
2022-11-30 20:01:23 +0900

    feat: implement feature to paginate image files in app's home view

---
+++ src/main/java/com/eunbinlib/api/dto/request/ImageReadRequest.java
@@ -0,0 +1,30 @@
+package com.eunbinlib.api.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import static java.lang.Math.min;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ImageReadRequest {
+
+    public static final int DEFAULT_SIZE = 20;
+    public static final int MAX_SIZE = 100;
+
+    private Long after;
+
+    private Integer size;
+
+    public Integer getSize() {
+        return size != null ? size : DEFAULT_SIZE;
+    }
+
+    public long getLimit() {
+        return min(getSize(), MAX_SIZE);
+    }
+}
